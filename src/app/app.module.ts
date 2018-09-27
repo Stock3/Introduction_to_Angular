49de097df9a55cb9ff863a70ff40dd6509e0a17f
Alex_Stock <olexiy.mitskan@gmail.com>
2018-09-27 13:11:22 +0300

    Angular Components 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
@@ -18,6 +21,9 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,  
     MatToolbarModule,
     FlexLayoutModule
   ],
