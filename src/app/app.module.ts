ded75e5e60f2c45f9d492c542dc84696d47159c6
Alex_Stock <olexiy.mitskan@gmail.com>
2018-10-09 12:45:00 +0300

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatToolbarModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
