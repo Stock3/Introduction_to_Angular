4543b6f5bc1b3892792f34d16a608302de1f9ae3
Alex_Stock <olexiy.mitskan@gmail.com>
2018-10-11 13:18:14 +0300

    Single Page Application Part 1

--- src/app/shared/promotion.ts
+++ src/app/shared/promotion.ts
@@ -1,13 +1,9 @@
-import { Comment } from './comment';
-
-export class Dish{
+export class Promotion{
     id: string;
     name: string;
     image: string;
-    category: string;
     label: string;
     price: string;
     featured: boolean;
     description: string;
-    comments: Comment[];
 }
